fe486e48e239d660e4793801767ee87be92dd2b6
kwangphys <anothermat@gmail.com>
2018-06-15 23:15:18 +0800

    add task to write nasdaq calendar into db

--- yahoo/models.py
+++ yahoo/models.py
@@ -1,11 +1,11 @@
-from django.db import models
+from django.db import models as dm
 from getpass import getuser
 
 
-class DatedModel(models.Model):
+class DatedModel(dm.Model):
 
-    updated_on = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=64, default=getuser())
+    updated_on = dm.DateTimeField(auto_now_add=True)
+    updated_by = dm.CharField(max_length=64, default=getuser())
 
     class Meta:
         abstract = True
@@ -13,12 +13,12 @@ class DatedModel(models.Model):
 
 class NasdaqEarningsCalendar(DatedModel):
 
-    ticker = models.CharField(max_length=16, null=False, blank=False)
-    full_name = models.CharField(max_length=255)
-    fiscal_quarter_ending = models.CharField(max_length=16, null=False, blank=False)
-    earnings_date = models.DateField(null=False)
-    time = models.CharField(max_length=32)
-    consensus_eps_forecast = models.FloatField()
-    n_estimates = models.IntegerField()
-    is_confirmed = models.BooleanField()
+    ticker = dm.CharField(max_length=16, null=False, blank=False)
+    full_name = dm.CharField(max_length=255)
+    fiscal_quarter_ending = dm.CharField(max_length=16, null=False, blank=False)
+    earnings_date = dm.DateField(null=False)
+    time = dm.CharField(max_length=32)
+    consensus_eps_forecast = dm.FloatField(null=True)
+    n_estimates = dm.IntegerField()
+    is_confirmed = dm.BooleanField()
 
--- yahoo/tasks.py
+++ yahoo/tasks.py
@@ -1,29 +1,85 @@
-if __name__ == '__main__':
-    import pickle
-    import os
-    import datetime
-    from selenium import webdriver
-    from er_nasdaq import *
-    from yahoo import *
+import pickle
+import os
+import datetime
+from yahoo.models import *
+from selenium import webdriver
+from yahoo.er_nasdaq import *
+from yahoo.yahoo import *
 
-    date = datetime.datetime(2018, 6, 11)
-    folder = "E:\\Data\\USFundamentals"
+
+def get_nasdaq_er_filename(folder, date):
+    datestr = date.strftime('%Y%m%d')
+    return os.path.join(folder, 'er_nasdaq_' + datestr + '.pkl')
+
+
+def get_yahoo_fundamental_filename(folder, date, ticker):
+    datestr = date.strftime('%Y%m%d')
+    subfolder = os.path.join(folder, datestr)
+    return os.path.join(subfolder, ticker + '.pkl')
+
+
+def scrape_raw_data(folder, date):
     browser = webdriver.Chrome()
     ercal = get_nasdaq_earnings_calendar(date, browser)
     datestr = date.strftime('%Y%m%d')
-    pickle.dump(ercal, open(os.path.join(folder, 'er_nasdaq_' + datestr + '.pkl'), 'wb'))
+    pickle.dump(ercal, open(get_nasdaq_er_filename(folder, date), 'wb'))
     subfolder = os.path.join(folder, datestr)
     if not os.path.exists(subfolder):
         os.makedirs(subfolder)
     for info in ercal['confirmed']:
         ticker = info['ticker']
+        ticker_filename = get_yahoo_fundamental_filename(folder, date, ticker)
         print('Processing', ticker)
         results = parse_yahoo_data(ticker, browser)
-        pickle.dump(results, open(os.path.join(subfolder, ticker + '.pkl'), 'wb'))
+        pickle.dump(results, open(ticker_filename, 'wb'))
     for info in ercal['unconfirmed']:
         ticker = info['ticker']
+        ticker_filename = get_yahoo_fundamental_filename(folder, date, ticker)
         print('Processing', ticker)
         results = parse_yahoo_data(ticker, browser)
-        pickle.dump(results, open(os.path.join(subfolder, ticker + '.pkl'), 'wb'))
+        pickle.dump(results, open(ticker_filename, 'wb'))
     browser.quit()
     print('Done')
+
+
+def nasdaq_earnings_calendar_to_db(folder, date):
+    filename = get_nasdaq_er_filename(folder, date)
+    create_time = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
+    data = pickle.load(open(filename, 'rb'))
+    for item in data['confirmed']:
+        record = NasdaqEarningsCalendar(
+            updated_on=create_time,
+            is_confirmed=True,
+            ticker=item['ticker'],
+            full_name=item['full_name'],
+            fiscal_quarter_ending=item['fiscal_quarter_ending'],
+            earnings_date=item['earnings_date'].date(),
+            time=item['time'],
+            consensus_eps_forecast=item['consensus_eps_forecast'],
+            n_estimates=item['n_estimates']
+        )
+        record.save()
+    for item in data['unconfirmed']:
+        record = NasdaqEarningsCalendar(
+            updated_on=create_time,
+            is_confirmed=False,
+            ticker=item['ticker'],
+            full_name=item['full_name'],
+            fiscal_quarter_ending=item['fiscal_quarter_ending'],
+            earnings_date=item['earnings_date'].date(),
+            consensus_eps_forecast=item['consensus_eps_forecast'],
+            n_estimates=item['n_estimates'],
+            time='N/A'
+        )
+        record.save()
+
+
+if __name__ == '__main__':
+    import pickle
+    import os
+    import datetime
+
+    date = datetime.datetime(2018, 6, 11)
+    folder = "X:\\Trading\\USFundamentals"
+    # scrape_raw_data(folder, date)
+    nasdaq_earnings_calendar_to_db(folder, date)
