e018d784c4e665a5ed4630bf37e02a7191943cd8
kwangphys <anothermat@gmail.com>
2018-09-01 23:44:08 +0800

    improve scrape post er calendars

--- scripts/scrape_post_er_calendars.py
+++ scripts/scrape_post_er_calendars.py
@@ -3,18 +3,22 @@ from yahoo.yahoo import *
 from yahoo.er_nasdaq import *
 
 
-def scrape_one_day(d, folder, browser):
+def scrape_one_day(d, folder):
+    browser = webdriver.Chrome()
     browser.set_page_load_timeout(30)
     data = get_nasdaq_post_earnings_calendar(d, browser)
     if len(data) > 0:
         pickle.dump(data, open(get_nasdaq_post_er_filename(folder, d), 'wb'))
         nasdaq_post_earnings_calendar_to_db(folder, d)
+    browser.quit()
 
+    browser = webdriver.Chrome()
     browser.set_page_load_timeout(30)
     data = parse_yahoo_post_earnings_calendar(d, browser)
     if len(data) > 0:
         pickle.dump(data, open(get_yahoo_post_er_filename(folder, d), 'wb'))
         yahoo_post_earnings_calendar_to_db(folder, d)
+    browser.quit()
 
 
 if __name__ == '__main__':
@@ -22,11 +26,10 @@ if __name__ == '__main__':
     start_date = datetime.datetime(2018, 6, 8)
     end_date = datetime.datetime.today()
     folder = "E:\\Data\\USFundamentals"
-    browser = webdriver.Chrome()
     d = start_date
     while d < end_date:
-        scrape_one_day(d, folder, browser)
+        print(d)
+        scrape_one_day(d, folder)
         d = d + datetime.timedelta(days=1)
-    browser.quit()
     print('Done')
 
